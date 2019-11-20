a9dc5061964db1db7674bf4e2f076a58ed750c84
Roman Gruba <roman.gruba@gmail.com>
2019-11-20 23:43:21 +0200

    Updates

---
+++ precache-manifest.8b77c1106137cbdcc54405a60189d5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf543d864c5ca6aae5169980262f03d",
+    "url": "/miz/index.html"
+  },
+  {
+    "revision": "fd53d2be2345986d9067",
+    "url": "/miz/static/css/main.2dbf110b.chunk.css"
+  },
+  {
+    "revision": "379cc7bef8ca6ed12805",
+    "url": "/miz/static/js/2.1f6ece3a.chunk.js"
+  },
+  {
+    "revision": "fd53d2be2345986d9067",
+    "url": "/miz/static/js/main.21df185f.chunk.js"
+  },
+  {
+    "revision": "8fb46f92d2496c68b8ae",
+    "url": "/miz/static/js/runtime-main.069a3185.js"
+  },
+  {
+    "revision": "3d0357d3f2731a2b0cf618311a155784",
+    "url": "/miz/static/media/Homepage.3d0357d3.jpg"
+  }
+]);
