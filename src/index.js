d3825cc3ee7fa70281caa7827931c7bfccb827ca
Roman Gruba <roman.gruba@gmail.com>
2019-11-20 23:47:59 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <HashRouter basename="/miz">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
