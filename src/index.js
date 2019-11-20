0ce1e91256a6afcf4700f741ab49cd333c79f596
Roman Gruba <roman.gruba@gmail.com>
2019-11-20 23:50:29 +0200

    hash

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter basename="/miz">
+  <HashRouter basename="/">
     <App />
   </HashRouter>,
   document.getElementById("root")
